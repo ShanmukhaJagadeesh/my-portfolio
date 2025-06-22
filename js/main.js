a8723e6e962736cb2d0ace9b57b57ec2d3d900ad
shanmukhPunnam <shanmukhajagadeesh@gmail.com>
2025-06-22 23:15:16 +0530

    added skills icons

--- js/main.js
+++ js/main.js
@@ -3,11 +3,18 @@ import { renderStats }      from './stats.js';
 import { initTimeline }     from './timeline.js';
 import { initProjects }     from './projects.js';
 
+async function loadSkillsSection() {
+    const res = await fetch('html/skills.html');
+    const html = await res.text();
+    document.getElementById('skills-container').innerHTML = html;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
 
     animateTagline();
     renderStats();
     initTimeline();
     initProjects();
+    loadSkillsSection();
 });
 
